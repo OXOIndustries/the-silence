691e7558d2413e0a01b0c63d273ae084cef64af9
Gedan <gedan@ganked.me>
2014-09-08 21:56:40 +0100

    Get the fuck out of here Typescript.

--- game/Preloader.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Created by Gedan on 07/09/2014.
- */
-
-///<reference path="../lib/createjs.d.ts"/>
-
-export var Assets:createjs.LoadQueue = new createjs.LoadQueue(false);
-
--- game/globals.ts
+++
@@ -1,5 +0,0 @@
-/**
- * Created by Gedan on 07/09/2014.
- */
-
-export var gameVersion:string = "0.00.01";
